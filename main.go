81dec805b30e2c678b1382919f091be4e9581e36
sagalm96 <zmohamed6991@gmail.com>
2025-01-07 12:02:02 +0000

    calculating func

--- main.go
+++ main.go
@@ -9,8 +9,16 @@ func callingFunc() {
     fmt.Println("Calling function")
 }
 
+func calculatingFunc(a int, b int) int {
+    return a + b
+}
+
+
 func main() {
 
     callingFunc()
+    fmt.Println(calculatingFunc(5,4))
+
 
 }
+
